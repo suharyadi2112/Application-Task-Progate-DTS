345a8e4a1f5465958b5e276aa7314fc4ea2a31a4
suharyadi <suharyadi21@outlook.com>
2022-08-07 12:24:58 +0700

    + add koneksi postgres

---
+++ be/connection/postgres.go
@@ -0,0 +1,39 @@
+package connect
+
+import(
+	"fmt"
+    "database/sql"
+    _ "github.com/lib/pq"
+    "os"
+)
+
+func Dbcon() (*sql.DB, error) {
+
+    // Set Environment Variables
+    os.Setenv("SITE_TITLE", "Tegars Home")
+    os.Setenv("DB_HOST", "localhost")
+    os.Setenv("DB_PORT", "5432")
+    os.Setenv("DB_USERNAME", "postgres")
+    os.Setenv("DB_PASSWORD", "12345678")
+    os.Setenv("DB_NAME", "golearn")
+
+    // Get the value of an Environment Variable
+    host := os.Getenv("DB_HOST")
+    port := os.Getenv("DB_PORT")
+    user := os.Getenv("DB_USERNAME")
+    password := os.Getenv("DB_PASSWORD")
+    dbname := os.Getenv("DB_NAME")
+
+    //https://zetcode.com/golang/string-format/ (Referensi string format %s, %d, dsb)
+
+    psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
+    "password=%s dbname=%s sslmode=disable",
+    host, port, user, password, dbname)
+
+    db, err := sql.Open("postgres", psqlInfo)
+    if err != nil {
+        return nil, err
+    }
+
+    return db, nil
+}
