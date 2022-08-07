e68a78cc3091341272b529bf458740a9a1144221
suharyadi <suharyadi21@outlook.com>
2022-08-07 12:32:34 +0700

    - update route file

--- be/main.go
+++ be/main.go
@@ -1,1 +1,20 @@
-be
+package main 
+
+import(
+	"net/http"
+
+	b "be/task/task_process"//mengimport file lain untuk digunakan functionnya
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main(){
+	
+	r := chi.NewRouter()//Chi Route
+	r.Use(middleware.Logger)
+
+	r.Get("/task", b.Gettask)// get all task
+	
+	http.ListenAndServe(":8080", r)//web service running
+}
