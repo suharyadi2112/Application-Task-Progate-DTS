5a4c279d4d9b2876f6ec3463444f1b6220c9be96
suharyadi <suharyadi21@outlook.com>
2022-08-10 03:37:08 +0700

    - ganti routing chi menjadi mux

--- be/main.go
+++ be/main.go
@@ -5,24 +5,32 @@ import(
 	"fmt"
 
 	b "be_progate_task/process"//mengimport file lain untuk digunakan functionnya
-
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/gorilla/mux"//mux route
 )
 
 func main(){
 
-	r := chi.NewRouter()//Chi Route
-	r.Route("/progatedts", func(r chi.Router) {
-		r.Use(middleware.Logger)
-		r.Get("/task", b.Gettask)//all task
-		r.Get("/task_id/{userID}", b.Gettask_byid)//task by id
-		r.Post("/task_post", b.PostTask)//post task
-		r.Delete("/task_del/{userID}", b.DelTask_id)//delete task
-		r.Put("/task_up/{userID}", b.UpTask_id)//update task
-		r.Put("/task_changestatus/{userID}", b.ChangeStatusTask)//update status task
+	k := mux.NewRouter()
 
-	})
+    // IMPORTANT: you must specify an OPTIONS method matcher for the middleware to set CORS headers
+    k.HandleFunc("/task_changestatus/{userID}", b.ChangeStatusTask).Methods("GET")
+    k.HandleFunc("/task", b.Gettask).Methods("GET")
+    k.HandleFunc("/task_id/{userID}", b.Gettask_byid).Methods("GET")
+    k.HandleFunc("/task_post", b.PostTask).Methods("POST")
+    k.HandleFunc("/task_del/{userID}", b.DelTask_id).Methods("GET")
+    k.HandleFunc("/task_up/{userID}", b.UpTask_id).Methods(http.MethodGet, http.MethodPut, http.MethodPatch, http.MethodOptions)
+    	
+    k.Use(loggingMiddleware)
+
 	fmt.Println("server started at localhost:9999")
-	http.ListenAndServe(":9999", r)//web service running
+	http.ListenAndServe(":9999", k)//web service running
+}
+
+func loggingMiddleware(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        // Do stuff here
+        fmt.Println(r.RequestURI)
+        // Call the next handler, which can be another middleware in the chain, or the final handler.
+        next.ServeHTTP(w, r)
+    })
 }
