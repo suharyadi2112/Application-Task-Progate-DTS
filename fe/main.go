6b6088b3bf19f06f1bcba9bd05f6cc94aa200881
suharyadi <suharyadi21@outlook.com>
2022-08-10 03:34:44 +0700

    - update route edit

--- fe/main.go
+++ fe/main.go
@@ -4,16 +4,26 @@ import(
     "fmt"
     "net/http"
     "html/template"
-    // "path"   
     "github.com/go-chi/chi/v5"
     "github.com/go-chi/chi/v5/middleware"
-)
+ )
 
 //home
 func home(w http.ResponseWriter, r *http.Request){
     var tmpl = template.Must(template.New("index").ParseFiles("index.html"))
-
     if err := tmpl.Execute(w, nil); err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+}
+//edit
+func edit(w http.ResponseWriter, r *http.Request){
+
+    Taskid := chi.URLParam(r, "task_id")
+    var tmpl = template.Must(template.New("edit").ParseFiles("edit.html"))
+    var data_id = map[string]interface{}{
+        "id_task": Taskid,
+    }
+    if err := tmpl.Execute(w, data_id); err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
     }
 }
@@ -27,6 +37,7 @@ func main() {
     //start routing
     r.Get("/", home)//home
     r.Get("/home", home)//home
+    r.Get("/edit/{task_id}", edit)//home
 
     fmt.Println("server started at localhost:9000")
     http.ListenAndServe(":9000", r)
